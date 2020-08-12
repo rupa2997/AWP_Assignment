4636548903c7c229af5b99467a11bcb93778bbba
rupa2997 <rupali2997pangare@gmail.com>
2020-08-13 01:40:58 +0530

    Assignments

---
+++ A5/hover.js
@@ -0,0 +1,11 @@
+function mousehover1() {
+    var id1Element = document.querySelector("#id1");
+    id1Element.innerHTML = "Hello Rupali Pangare...";
+    id1Element.style.background = "black";
+    id1Element.padding = "12px";
+}
+function mouseout1() {
+    let id1Element = document.querySelector("#id1");
+    id1Element.innerHTML = "Hi...........";
+    id1Element.style.background = "Pink";
+}
